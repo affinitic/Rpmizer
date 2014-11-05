6586749f68d04d55c17a9243f52a8500401f480a
Godefroid Chapelle <gotcha@bubblenet.be>
2014-11-05 12:36:16 +0100

    do not try to move files with *

--- run_buildout.sh
+++ run_buildout.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -e
 
 if [ $# -lt 4 ]; then
     echo "Run buildout awaits path_to_python, rpm_build_directory, install_dir, and buildut root passed as arguments"
@@ -13,7 +13,7 @@ BUILDOUT_CFG=$4
 "$PATH_TO_PYTHON" bootstrap.py -c "$BUILDOUT_CFG"
 bin/buildout -N -c "$BUILDOUT_CFG" install download
 bin/buildout -N -c "$BUILDOUT_CFG" install install
-mkdir -p "$INSTALL_DIR/downloads" "$INSTALL_DIR/eggs"
-mv "$RPM_BUILD_DIR/buildout-cache/downloads/"* "$INSTALL_DIR/downloads/"
-mv "$RPM_BUILD_DIR/buildout-cache/eggs/"* "$INSTALL_DIR/eggs/"
+mkdir -p "$INSTALL_DIR"
+mv "$RPM_BUILD_DIR/buildout-cache/downloads" "$INSTALL_DIR"
+mv "$RPM_BUILD_DIR/buildout-cache/eggs" "$INSTALL_DIR"
 bin/buildout -N -c "$BUILDOUT_CFG" buildout:directory="$INSTALL_DIR"

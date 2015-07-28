39864b2b87e878965ac61474769bf48fd207c677
Godefroid Chapelle <gotcha@bubblenet.be>
2015-07-28 14:09:35 +0200

    pass zc.buildout and setuptools version when running buildout.
    to shadow versions set in dist.plone.org/release/...

--- run_buildout.sh
+++ run_buildout.sh
@@ -13,9 +13,11 @@ BUILDOUT_VERSION=$5
 SETUPTOOLS_VERSION=$6
 GSC_ORACLE=$7
 
+BIN_BUILDOUT="bin/buildout versions:setuptools=$SETUPTOOLS_VERSION versions:zc.buildout=$BUILDOUT_VERSION"
+
 "$PATH_TO_PYTHON" bootstrap-buildout.py -c "$BUILDOUT_CFG" --buildout-version="$BUILDOUT_VERSION" --setuptools-version="$SETUPTOOLS_VERSION"
-bin/buildout -N -c "$BUILDOUT_CFG" install download
-bin/buildout -N -c "$BUILDOUT_CFG" install install
+$BIN_BUILDOUT -N -c "$BUILDOUT_CFG" install download
+$BIN_BUILDOUT -N -c "$BUILDOUT_CFG" install install
 mkdir -p "$INSTALL_DIR"
 mv "$RPM_BUILD_DIR/buildout-cache/downloads" "$INSTALL_DIR"
 mv "$RPM_BUILD_DIR/buildout-cache/eggs" "$INSTALL_DIR"
@@ -26,8 +28,8 @@ if [ "$GSC_ORACLE" == "true" ]; then
   (
   export ORACLE_HOME="$INSTALL_DIR/parts/python-oracle"
   export LD_LIBRARY_PATH="$ORACLE_HOME"
-  bin/buildout -N -c "$BUILDOUT_CFG" buildout:directory="$INSTALL_DIR"
+  $BIN_BUILDOUT -N -c "$BUILDOUT_CFG" buildout:directory="$INSTALL_DIR"
   )
 else
-  bin/buildout -N -c "$BUILDOUT_CFG" buildout:directory="$INSTALL_DIR"
+  $BIN_BUILDOUT -N -c "$BUILDOUT_CFG" buildout:directory="$INSTALL_DIR"
 fi
